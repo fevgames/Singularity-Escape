4d597eb6bd6802c94892e4946fee062e2962b0a7
FevGames <fevplaysgames@gmail.com>
2015-12-26 23:55:16 +0100

    Tiled map loader test

--- core/src/com/fevgames/singularityescape/screens/GameScreen.java
+++ core/src/com/fevgames/singularityescape/screens/GameScreen.java
@@ -1,24 +1,30 @@
 package com.fevgames.singularityescape.screens;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.fevgames.singularityescape.map.MapTile;
 
 /**
  * Created by Roby on 21/12/2015.
  */
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
 
     SpriteBatch batch;
     Game game;
     OrthographicCamera camera;
 
-    MapTile[] mapTile;
+    TiledMapRenderer tiledMapRenderer;
+    TiledMap tiledMap;
+
+
 
     //Texture tile;
     //float tileW,tileH;
@@ -32,17 +38,11 @@ public class GameScreen implements Screen {
         //tileW=1*((float)tile.getWidth()/(float)tile.getHeight());
 
         batch = new SpriteBatch();
-        mapTile=new MapTile[3];
-        for(int x=0;x<3;x++)
-        {
-            for(int y=0;y<1;y++)
-            {
-                mapTile[x+((y*3))]=new MapTile("tiles/brickpavers2.png");
-                mapTile[x+((y*3))].setPos(x-1,y-1);
-            }
-        }
+        tiledMap = new TmxMapLoader().load("map/iso.tmx");
+        tiledMapRenderer = new IsometricTiledMapRenderer(tiledMap);
+        Gdx.input.setInputProcessor(this);
 
-
+        Gdx.app.log("SE","GameScreen::GameScreen()");
     }
 
     @Override
@@ -53,37 +53,43 @@ public class GameScreen implements Screen {
     @Override
     public void render(float delta) {
 
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        if(Gdx.input.isKeyJustPressed(Input.Keys.PLUS))
+        {
+            camera.zoom-=0.2;
+        }
+        if(Gdx.input.isKeyJustPressed(Input.Keys.MINUS))
+        {
+            camera.zoom+=0.2;
+            //Gdx.app.log("SE","Pos("+camera.position.x+","+camera.position.y+")");
+        }
+
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
+        /*batch.setProjectionMatrix(camera.combined);
+        batch.begin();*/
 
-        for(int i=0;i<3;i++)
-        {
-            mapTile[i].draw(batch);
-        }
-
-        /*batch.draw(tile, -(tileW/2), -(tileH/2), tileW, tileH, 0, 0,
-                tile.getWidth(), tile.getHeight(), false, false);
-
-        batch.draw(tile, -(tileW/2)+(tileW/2), -(tileH/2+(tileH/2)), tileW, tileH, 0, 0,
-                tile.getWidth(), tile.getHeight(), false, false);*/
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
 
 
 
-        batch.end();
+
+        //batch.end();
 
     }
 
     @Override
     public void resize(int width, int height) {
+        Gdx.app.log("SE","GameScreen::resize("+width+","+height+")");
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
         //camera = new OrthographicCamera(4,4*( h / w));
-        camera = new OrthographicCamera(5*(w/h),5);
-        camera.position.set(0f, 0f,0);
+        camera = new OrthographicCamera(w,h);
+        camera.position.set(1508.0f,932.0f,0);
+        camera.zoom=2;
     }
 
     @Override
@@ -104,5 +110,55 @@ public class GameScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+
+        if(keycode == Input.Keys.LEFT)
+            camera.translate(-32,0);
+        if(keycode == Input.Keys.RIGHT)
+            camera.translate(32,0);
+        if(keycode == Input.Keys.UP)
+            camera.translate(0,-32);
+        if(keycode == Input.Keys.DOWN)
+            camera.translate(0,32);
+
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
